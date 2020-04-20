366fab656dfaa7d0b9b095706c1a012956419575
RazviM12 <batmaninthefuture@gmail.com>
2020-04-20 18:52:43 +0300

    Create _Code_Study_2_Postfix_increment.java

---
+++ _1_basics_Razvan_Muresan/_code_study/_Code_Study_2_Postfix_increment.java
@@ -0,0 +1,16 @@
+public class Postfix_increment {
+
+    public static void main(String[] args){
+        int x = 0;
+        while (x++ < 10){
+            checkForXValue(x);
+        }
+    }
+
+    private static void checkForXValue(int x){
+        int expectedXValue = 11;
+
+        System.out.println("Guessed correctly the value of x ? -" +(x == expectedXValue));
+    }
+
+}
