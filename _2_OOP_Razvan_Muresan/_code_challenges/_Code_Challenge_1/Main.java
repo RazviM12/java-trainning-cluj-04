a67faf222635f4354cdda601d398bd67b0ad8d45
RazviM12 <batmaninthefuture@gmail.com>
2020-04-24 13:16:41 +0300

    Create Main.java

---
+++ _2_OOP_Razvan_Muresan/_code_challenges/_Code_Challenge_1/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        BoxingMatch c = new BoxingMatch();
+        System.out.println("The winner is: " + c.winner);
+
+    }
+
+}
